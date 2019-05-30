fc46d424fb6e42604bf27943a96ba0543f3aac29
Nezud <nezud@hotmail.com>
2019-05-30 09:21:18 +0300

    Formatting

--- bot.py
+++ bot.py
@@ -50,16 +50,21 @@ async def on_message(message):
     if message.content == '!hello':
         author = message.author.mention
         msg = f'Hello {author}, type !commands for available commands.'
+
         await message.channel.send(msg)
+
     elif message.content.startswith("!stats"):
         keywords = message.content.split()
         kw_string = ""
         length = len(keywords)
+
         for i in range(length)[1:]:
             kw_string += f"{keywords[i]}+"
+        
         url = f'https://classic.wowhead.com/item={kw_string[:-1]}&xml'
         r = requests.get(url)
         response_dict = xmltodict.parse(r.content)
+        
         if not response_dict.get("wowhead").get("error"):
             jsone = response_dict['wowhead']['item']['json']
             item_json = f'{{{jsone}}}'
@@ -69,71 +74,98 @@ async def on_message(message):
             msg = f'{response_str}'
         else:
             msg = "Item not found"
+    
         await message.channel.send(msg)
+
     elif message.content == "!commands":
         embed = discord.Embed(title="WoWHypeBot's commands", description="Here are all of the WoWHypeBot's commands.")
         for key, val in command_list_dict.items():
             embed.add_field(name=f"{key}", value=f"{val}")
+
         await message.channel.send(content=None, embed=embed)
+
     elif message.content.lower() == '!groovy':
         msg = f"-play youtube_link to play a youtube video's audio.\n" \
             f"-stop to stop playing audio.\n"\
             f"-help to get further help from Groovy.\n"
+
         await message.channel.send(msg)
+
     elif message.content.startswith('!duckit'):
         keywords = message.content.split()
         kw_string = ""
         length = len(keywords)
+
         for i in range(length)[1:]:
             kw_string += f"{keywords[i]}+"
+        
         url = f'{duckduckgo}{kw_string[:-1]}'
         msg = f'{url}'
+
         await message.channel.send(msg)
+
     elif message.content.startswith('!youtube'):
         keywords = message.content.split()
         kw_string = ""
         length = len(keywords)
+        
         for i in range(length)[1:]:
             kw_string += f"{keywords[i]}+"
+        
         url = f'"https://www.youtube.com/results?search_query="{kw_string[:-1]}'
         msg = f'{url}'
+
         await message.channel.send(msg)
+
     elif message.content.startswith('!wowhead'):
         keywords = message.content.split()
         kw_string = ""
         length = len(keywords)
+        
         for i in range(length)[1:]:
             kw_string += f"{keywords[i]}+"
+        
         url = f'https://classic.wowhead.com/search?q={kw_string[:-1]}'
         msg = f'{url}'
+
         await message.channel.send(msg)
+
     elif message.content.lower() == '!blues':
         msg = f"{blue_tracker}"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!reddit':
         msg = f"{reddit_url}"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!streams':
         msg = f"{wow_streams}"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!wowmusic':
         msg = f"{wow_soundtrack}"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!map':
         msg = f"{wow_map_url}"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!classicresources':
         msg = f"{classic_resources}"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!classic':
         msg = "https://classic.wowhead.com/guides/classic-wow-classes-and-talent-overview"
         await message.channel.send(msg)
+
     elif message.content.lower() == '!talents':
         msg = "https://classic.wowhead.com/talent-calc"
         await message.channel.send(msg)
+
     elif message.content.lower() == "!release":
         msg = f'{time_to_release}.'
         await message.channel.send(msg)
+
     elif message.content.lower() == '!druidguide':
         msg = f"https://www.warcrafttavern.com/guides/taladrils-treatise-on-druid-tanking-in-vanilla/"
         await message.channel.send(msg)
